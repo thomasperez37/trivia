320fc7927c4bdb1c686ee03d3cb3bc208c8b544f
Patricia Perez <patricia.perez0301@gmail.com>
2019-01-30 23:06:06 +0000

    added styling to answer buttons

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import AnswerButton from './AnswerButton';
 import QuestionText from './QuestionText';
 import ResetButton from './ResetButton';
+import '../css/Question.css';
 
 class Question extends Component {
   
@@ -11,11 +12,13 @@ class Question extends Component {
       
     return (
       <div className="question">
-         <QuestionText question={question.title}/>
-         {question.answers.map(answerText => (
-          <AnswerButton answerText={answerText}/>
-        ))}
-         <ResetButton/>
+        <QuestionText question={question.title}/>
+        <div className= "answers">
+            {question.answers.map(answerText => (
+              <AnswerButton answerText={answerText}/>
+            ))}
+        </div>
+        <ResetButton/>
       </div>
     );
   }
